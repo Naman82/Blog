9612634f755db8091ef534a90598beecdfe7251c
Naman82 <b520031@iiit-bh.ac.in>
2022-04-17 11:36:47 +0530

    UI changed

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 urlpatterns=[
     path('',views.home,name='home'),
     path('profile/',views.profile,name='profile'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -7,3 +7,9 @@ def home(request):
 
 def profile(request):
     return render(request,'profile.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
